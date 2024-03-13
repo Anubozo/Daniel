b86b134f0754957a982c585980114e7a79813e1c
Anubhav <bist.anubhav0@gmail.com>
2024-03-13 00:36:15 -0400

    Added interaction distance limit

--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
+
+    public float interactionDistance = 10;
     
     // Start is called before the first frame update
     void Start()
@@ -14,44 +16,42 @@ public class PlayerInteraction : MonoBehaviour
 
     void Update()
     {
-        Vector3 forward = Quaternion.Euler(transform.GetChild(0).gameObject.transform.rotation.eulerAngles.x,0,0)*transform.TransformDirection(Vector3.forward)*5f;
+        Vector3 forward = Quaternion.Euler(transform.GetChild(0).gameObject.transform.rotation.eulerAngles.x,0,0)*transform.TransformDirection(Vector3.forward)*2f;
         
         Debug.DrawRay(transform.position, forward, Color.yellow);
 
         if(Input.GetKeyDown(KeyCode.E)){
             RaycastHit hit;
             if (Physics.Raycast(transform.position, forward, out hit)){
-                GameObject obj = hit.collider.gameObject;
-                Debug.Log(obj.name);
-                int CollisionLayer = LayerMask.NameToLayer("InteractionLayer");
-                if(obj.layer == CollisionLayer){
+                if(hit.distance<=interactionDistance){
+                    GameObject obj = hit.collider.gameObject;
+                    Debug.Log(obj.name);
+                    int CollisionLayer = LayerMask.NameToLayer("InteractionLayer");
+                    if(obj.layer == CollisionLayer){
 
-                    // Interact with door
-                    if(obj.name=="DoorTexture"){
-                        GameObject hitbox = obj.transform.parent.transform.GetChild(1).gameObject;     
-                        if(obj.transform.localRotation==Quaternion.Euler(0,90,0)){ 
-                            obj.transform.localRotation= Quaternion.Euler(0,0,0);
-                            obj.transform.localPosition= new Vector3(0, 0, 0);
-                        } else {
-                            obj.transform.localRotation= Quaternion.Euler(0,90,0);
-                            obj.transform.localPosition = new Vector3(-0.205f, -2.7696e-08f, -0.209f);
-                        }
-                    }
-
-                    if(obj.name == "DoorHitbox"){
-                        GameObject texture = obj.transform.parent.transform.GetChild(0).gameObject;
-                        if(texture.transform.localRotation == Quaternion.Euler(0,90,0)){ 
-                            texture.transform.localRotation =Quaternion.Euler(0,0,0);
-                            texture.transform.localPosition = new Vector3(0, 0, 0);
-                        } else {
-                            texture.transform.localRotation = Quaternion.Euler(0,90,0); 
-                            texture.transform.localPosition = new Vector3(-0.205f, -2.7696e-08f, -0.209f);
+                        // Interact with door
+                        if(obj.name=="DoorTexture"){
+                            GameObject hitbox = obj.transform.parent.transform.GetChild(1).gameObject;     
+                            if(obj.transform.localRotation==Quaternion.Euler(0,90,0)){ 
+                                obj.transform.localRotation= Quaternion.Euler(0,0,0);
+                                obj.transform.localPosition= new Vector3(0, 0, 0);
+                            } else {
+                                obj.transform.localRotation= Quaternion.Euler(0,90,0);
+                                obj.transform.localPosition = new Vector3(-0.205f, -2.7696e-08f, -0.209f);
+                            }
                         }
 
+                        if(obj.name == "DoorHitbox"){
+                            GameObject texture = obj.transform.parent.transform.GetChild(0).gameObject;
+                            if(texture.transform.localRotation == Quaternion.Euler(0,90,0)){ 
+                                texture.transform.localRotation =Quaternion.Euler(0,0,0);
+                                texture.transform.localPosition = new Vector3(0, 0, 0);
+                            } else {
+                                texture.transform.localRotation = Quaternion.Euler(0,90,0); 
+                                texture.transform.localPosition = new Vector3(-0.205f, -2.7696e-08f, -0.209f);
+                            }
+                        }
                     }
-
-
-                    // Interact with other thing:Vector3(0.00549999997,0.467599869,7.0999999) Vector3(2.88096166,4.10138988,0.199424356)
                 }
             }
         }
